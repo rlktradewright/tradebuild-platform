05c473f3666eb8365ba47523eff5c8726ecc73fb
Rlk <rlk@aultan.com>
2005-05-18 20:33:20 +0000

    Vss2Git

--- src/make.bat
+++ src/make.bat
@@ -1,1 +1,6 @@
+midl /mktyplib203 typelib\tradebuildsp.idl /out typelib
+regtlib typelib\tradebuildsp.tlb
+midl /mktyplib203 typelib\chartskiltypes.idl /out typelib
+regtlib typelib\chartskiltypes.tlb
 "e:\Program Files\Microsoft Visual Studio\VB98\vb6.exe" /make tradebuildplatform.vbg 
+pause
