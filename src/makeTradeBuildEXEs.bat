a38f799092366f240e6f1757c401163177d70f71
Rlk <rlk@aultan.com>
2007-04-14 21:53:11 +0000

    Vss2Git

--- src/makeTradeBuildEXEs.bat
+++ src/makeTradeBuildEXEs.bat
@@ -2,7 +2,9 @@ vb6 /m ChartDemo\chartdemo.vbp
 pause
 vb6 /m StudyTester\studytester.vbp
 pause
-vb6 /m TBDataCollectorUI\TBDataCollectorUI.vbp
+vb6 /m DataCollector\DataCollector.vbp
+pause
+vb6 /m TickfileManager\TickfileManager.vbp
 pause
 vb6 /m TradeSkilDemo\TradeSkilDemo.vbp
 pause
