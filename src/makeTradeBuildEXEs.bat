9a3061c7ad96aabc908f1272373d797306710a14
Rlk <rlk@aultan.com>
2011-08-12 11:54:39 +0000

    Vss2Git

--- src/makeTradeBuildEXEs.bat
+++ src/makeTradeBuildEXEs.bat
@@ -8,39 +8,49 @@ vb6 /m TickfileManager\TickfileManager.vbp
 rem pause
 vb6 /m TradeSkilDemo\TradeSkilDemo.vbp
 rem pause
-vb6 /m CommandLineUtils\gbd\gbd.vbp
+
+vb6 /m CommandLineUtils\gbd\gbd.vbp 
 rem pause
-link /EDIT /SUBSYSTEM:CONSOLE CommandLineUtils\gbd\gbd.exe
+link /EDIT /SUBSYSTEM:CONSOLE CommandLineUtils\gbd\gbd27.exe
+copy CommandLineUtils\gbd\gbd27.exe CommandLineUtils\
 
 vb6 /m CommandLineUtils\gccd\gccd.vbp
 rem pause
-link /EDIT /SUBSYSTEM:CONSOLE CommandLineUtils\gccd\gccd.exe
+link /EDIT /SUBSYSTEM:CONSOLE CommandLineUtils\gccd\gccd27.exe
+copy CommandLineUtils\gccd\gccd27.exe CommandLineUtils\
 
 vb6 /m CommandLineUtils\gcd\gcd.vbp
 rem pause
-link /EDIT /SUBSYSTEM:CONSOLE CommandLineUtils\gcd\gcd.exe
+link /EDIT /SUBSYSTEM:CONSOLE CommandLineUtils\gcd\gcd27.exe
+copy CommandLineUtils\gcd\gcd27.exe CommandLineUtils\
 
 vb6 /m CommandLineUtils\gtd\gtd.vbp
 rem pause
-link /EDIT /SUBSYSTEM:CONSOLE CommandLineUtils\gtd\gtd.exe
+link /EDIT /SUBSYSTEM:CONSOLE CommandLineUtils\gtd\gtd27.exe
+copy CommandLineUtils\gtd\gtd27.exe CommandLineUtils\
 
 vb6 /m CommandLineUtils\gxd\gxd.vbp
 rem pause
-link /EDIT /SUBSYSTEM:CONSOLE CommandLineUtils\gxd\gxd.exe
+link /EDIT /SUBSYSTEM:CONSOLE CommandLineUtils\gxd\gxd27.exe
+copy CommandLineUtils\gxd\gxd27.exe CommandLineUtils\
 
 vb6 /m CommandLineUtils\ltz\ltz.vbp
 rem pause
-link /EDIT /SUBSYSTEM:CONSOLE CommandLineUtils\ltz\ltz.exe
+link /EDIT /SUBSYSTEM:CONSOLE CommandLineUtils\ltz\ltz27.exe
+copy CommandLineUtils\ltz\ltz27.exe CommandLineUtils\
 
 vb6 /m CommandLineUtils\uccd\uccd.vbp
 rem pause
-link /EDIT /SUBSYSTEM:CONSOLE CommandLineUtils\uccd\uccd.exe
+link /EDIT /SUBSYSTEM:CONSOLE CommandLineUtils\uccd\uccd27.exe
+copy CommandLineUtils\uccd\uccd27.exe CommandLineUtils\
 
 vb6 /m CommandLineUtils\ucd\ucd.vbp
 rem pause
-link /EDIT /SUBSYSTEM:CONSOLE CommandLineUtils\ucd\ucd.exe
+link /EDIT /SUBSYSTEM:CONSOLE CommandLineUtils\ucd\ucd27.exe
+copy CommandLineUtils\ucd\ucd27.exe CommandLineUtils\
 
 vb6 /m CommandLineUtils\uxd\uxd.vbp
 rem pause
-link /EDIT /SUBSYSTEM:CONSOLE CommandLineUtils\uxd\uxd.exe
+link /EDIT /SUBSYSTEM:CONSOLE CommandLineUtils\uxd\uxd27.exe
+copy CommandLineUtils\uxd\uxd27.exe CommandLineUtils\
 
