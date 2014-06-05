fe235914918c0c87a203a76c821df008fe53d081
Rlk <rlk@aultan.com>
2014-06-05 17:27:09 +0000

    Vss2Git

--- src/DataCollector/CollectData.bat
+++ src/DataCollector/CollectData.bat
@@ -1,2 +1,2 @@
-start DataCollector27.exe /config:"Collect to DB" /log:log.log /showmonitor /posn:0,0 /startAt:Sun22:00 /endAt:Fri22:00 /exitAt:Fri22:05 /loglevel:N /noDataRestartSecs:300 /quantum:50
-rem start DataCollector27.exe /config:"Collect to files" /log:log.log /showmonitor /posn:0,0 /startAt:Sun22:00 /endAt:Fri22:00 /exitAt:Fri22:05 /loglevel:D /noDataRestartSecs:300 /quantum:50
+start DataCollector27.exe /config:"Collect to DB" /log:logDB.log /showmonitor /posn:0,1 /startAt:Sun22:00 /endAt:Fri22:00 /exitAt:Fri22:05 /loglevel:N /noDataRestartSecs:300 /quantum:50
+start DataCollector27.exe /config:"Collect to files" /log:logFiles.log /showmonitor /posn:0,0 /startAt:Sun22:00 /endAt:Fri22:00 /exitAt:Fri22:05 /loglevel:N /noDataRestartSecs:300 /quantum:50
