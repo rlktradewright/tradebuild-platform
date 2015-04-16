b57d8e5bcba9d152373eef9b8cff8425959afb32
Richard L King <rlking@aultan.com>
2015-04-16 18:05:34 +0100

    Improve build script

--- Build/makeExes.bat
+++ Build/makeExes.bat
@@ -5,6 +5,9 @@ path %TB-PLATFORM-PROJECTS-DRIVE%%TB-PLATFORM-PROJECTS-PATH%\..\Build;%TB-PLATFO
 
 set BIN-PATH=%TB-PLATFORM-PROJECTS-PATH%\..\Bin
 
+:: ========================================================
+:: Test projects
+::
 pushd %TB-PLATFORM-PROJECTS-PATH%\IBEnhancedAPI
 
 call makeExe ContractDataTest1
@@ -18,12 +21,20 @@ pushd %TB-PLATFORM-PROJECTS-PATH%\OrderUtils
 
 call makeExe OrdersTest1
 
+popd
+
 pushd %TB-PLATFORM-PROJECTS-PATH%\TradingUI
 
 call makeExe MarketChartTest1
 call makeExe TickerGridTest1
 
 popd
+
+::
+:: ========================================================
+:: Deliverable projects
+::
+pushd %TB-PLATFORM-PROJECTS-PATH%
 
 call makeExe ChartDemo
 call makeExe StudyTester
@@ -32,6 +43,12 @@ call makeExe TickfileManager
 call makeExe StrategyHost
 call makeExe TradeSkilDemo
 
+popd
+
+::
+:: ========================================================
+:: Command line utility projects
+::
 pushd %TB-PLATFORM-PROJECTS-PATH%\CommandLineUtils
 
 call makeExe gbd CONSOLE
