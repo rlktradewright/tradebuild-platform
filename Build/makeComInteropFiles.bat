b3f9b66ffd7a5005b1773504d41e9b35154228c3
Richard L King <rlking@aultan.com>
2017-10-31 17:18:28 +0000

    Suppress banner output from tlbimp and aximp

--- Build/makeComInteropFiles.bat
+++ Build/makeComInteropFiles.bat
@@ -131,14 +131,14 @@ exit /B 1
 
 :TlbImp
 echo =================================
-tlbimp "%SOURCE%\%1.dll" /out:Interop.%1.dll /tlbreference:"%TWWIN32API%" /namespace:%1 /silence:3011 /silence:3008 %ASM_VERSION% %REFERENCE%
+tlbimp "%SOURCE%\%1.dll" /out:Interop.%1.dll /tlbreference:"%TWWIN32API%" /namespace:%1 /nologo /silence:3011 /silence:3008 %ASM_VERSION% %REFERENCE%
 if errorlevel 1 goto :Err
 set REFERENCE=%REFERENCE% /reference:Interop.%1.dll
 echo.
 goto :EOF
 
 :TlbImpAx
-tlbimp "%SOURCE%\%1.ocx" /out:Interop.%1.dll /tlbreference:"%TWWIN32API%" /namespace:%1 /silence:3011 /silence:3008 %ASM_VERSION% %REFERENCE%
+tlbimp "%SOURCE%\%1.ocx" /out:Interop.%1.dll /tlbreference:"%TWWIN32API%" /namespace:%1 /nologo /silence:3011 /silence:3008 %ASM_VERSION% %REFERENCE%
 if errorlevel 1 goto :Err
 set REFERENCE=%REFERENCE% /reference:Interop.%1.dll
 goto :EOF
@@ -146,7 +146,7 @@ goto :EOF
 :AxImp
 echo =================================
 call :TlbImpAx %1
-aximp "%SOURCE%\%1.ocx" /out:Interop.Ax%1.dll /rcw:Interop.%1.dll
+aximp "%SOURCE%\%1.ocx" /out:Interop.Ax%1.dll /rcw:Interop.%1.dll /nologo
 if errorlevel 1 goto :Err
 echo.
 goto :EOF
