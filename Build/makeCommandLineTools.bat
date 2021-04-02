10badd5fa11a0987b52ccd0ce6f1919681060865
Richard L King <rlking@aultan.com>
2021-04-02 10:13:15 +0100

    Fix command line tool build file

--- Build/makeCommandLineTools.bat
+++ Build/makeCommandLineTools.bat
@@ -10,14 +10,11 @@ call setMyVersion.bat
 
 set DEP=/DEP:%TB-PLATFORM-PROJECTS-DRIVE%%TB-PLATFORM-PROJECTS-PATH%\Build\ExternalDependencies.txt
 
-if "%~1" == "V" (
-	set SET_VERSION=/V
-) else if not "%~1"=="" (
-	if "%2" == "V" set SET_VERSION=/V
+if not "%~1"=="" (
 	pushd %TB-PLATFORM-PROJECTS-PATH%\src\CommandLineUtils
 	echo =================================
 	echo Making command line utility project %~1
-	call makeExe.bat %~1 %~1 /CONSOLE /NOV6CC /M:E %DEP% /V
+	call makeExe.bat %~1 %~1 /CONSOLE /NOV6CC /M:E %DEP%
 	popd
 	exit /B
 )
@@ -27,41 +24,43 @@ echo Making command line utility projects
 
 pushd %TB-PLATFORM-PROJECTS-PATH%\src\CommandLineUtils
 
-call makeExe.bat gbd gbd /CONSOLE /NOV6CC /M:E %DEP% /V
+call makeExe.bat gbd gbd /CONSOLE /NOV6CC /M:E %DEP%
 if errorlevel 1 pause
 
-call makeExe.bat gccd gccd /CONSOLE /NOV6CC /M:E %DEP% /V
+call makeExe.bat gccd gccd /CONSOLE /NOV6CC /M:E %DEP%
 if errorlevel 1 pause
 
-call makeExe.bat gcd gcd /CONSOLE /NOV6CC /M:E %DEP% /V
+call makeExe.bat gcd gcd /CONSOLE /NOV6CC /M:E %DEP%
 if errorlevel 1 pause
 
-call makeExe.bat gsd gsd /CONSOLE /NOV6CC /M:E %DEP% /V
+call makeExe.bat gsd gsd /CONSOLE /NOV6CC /M:E %DEP%
 if errorlevel 1 pause
 
-call makeExe.bat gtd gtd /CONSOLE /NOV6CC /M:E %DEP% /V
+call makeExe.bat gtd gtd /CONSOLE /NOV6CC /M:E %DEP%
 if errorlevel 1 pause
 
-call makeExe.bat gxd gxd /CONSOLE /NOV6CC /M:E %DEP% /V
+call makeExe.bat gxd gxd /CONSOLE /NOV6CC /M:E %DEP%
 if errorlevel 1 pause
 
-call makeExe.bat ltz ltz /CONSOLE /NOV6CC /M:E %DEP% /V
+call makeExe.bat ltz ltz /CONSOLE /NOV6CC /M:E %DEP%
 if errorlevel 1 pause
 
-call makeExe.bat plord plord /CONSOLE /NOV6CC /M:E %DEP% /V
+call makeExe.bat plord plord /CONSOLE /NOV6CC /M:E %DEP%
 if errorlevel 1 pause
 
-call makeExe.bat uccd uccd /CONSOLE /NOV6CC /M:E %DEP% /V
+call makeExe.bat uccd uccd /CONSOLE /NOV6CC /M:E %DEP%
 if errorlevel 1 pause
 
-call makeExe.bat ucd ucd /CONSOLE /NOV6CC /M:E %DEP% /V
+call makeExe.bat ucd ucd /CONSOLE /NOV6CC /M:E %DEP%
 if errorlevel 1 pause
 
-call makeExe.bat uxd uxd /CONSOLE /NOV6CC /M:E %DEP% /V
+call makeExe.bat uxd uxd /CONSOLE /NOV6CC /M:E %DEP%
 if errorlevel 1 pause
 
 :: temporary solution to building the Chart program
 pushd Chart
+echo =================================
+echo Building Chart\Chart.exe
 msbuild Chart.sln -t:Rebuild -p:Configuration=Debug -verbosity:m
 if errorlevel 1 pause
 popd
