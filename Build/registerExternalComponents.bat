462bba4ee7c26bed0925be400990f3dd86c9fd5a
Richard L King <rlking@aultan.com>
2021-04-26 18:59:46 +0100

    Fix registering of external components

--- Build/registerExternalComponents.bat
+++ Build/registerExternalComponents.bat
@@ -4,6 +4,24 @@ setlocal
 %TB-PLATFORM-PROJECTS-DRIVE%
 pushd %TB-PLATFORM-PROJECTS-DRIVE%\%TB-PLATFORM-PROJECTS-PATH%\Bin\TradeWright.TradeBuild.ExternalComponents
 
+regsvr32 -S COMCT332.OCX
+if errorlevel 1 pause
+
+regsvr32 -S MSCOMCT2.OCX
+if errorlevel 1 pause
+
+regsvr32 -S MSDATGRD.OCX
+if errorlevel 1 pause
+
+regsvr32 -S MSFLXGRD.OCX
+if errorlevel 1 pause
+
+regsvr32 -S MSWINSCK.OCX
+if errorlevel 1 pause
+
+regsvr32 -S TABCTL32.OCX
+if errorlevel 1 pause
+
 regsvr32 -S TLBINF32.DLL
 if errorlevel 1 pause
 
--- Build/unregisterExternalComponents.bat
+++ Build/unregisterExternalComponents.bat
@@ -4,6 +4,24 @@ setlocal
 %TB-PLATFORM-PROJECTS-DRIVE%
 pushd %TB-PLATFORM-PROJECTS-DRIVE%\%TB-PLATFORM-PROJECTS-PATH%\Bin\TradeWright.TradeBuild.ExternalComponents
 
+regsvr32 -S -U COMCT332.OCX
+if errorlevel 1 pause
+
+regsvr32 -S -U MSCOMCT2.OCX
+if errorlevel 1 pause
+
+regsvr32 -S -U MSDATGRD.OCX
+if errorlevel 1 pause
+
+regsvr32 -S -U MSFLXGRD.OCX
+if errorlevel 1 pause
+
+regsvr32 -S -U MSWINSCK.OCX
+if errorlevel 1 pause
+
+regsvr32 -S -U TABCTL32.OCX
+if errorlevel 1 pause
+
 regsvr32 -S -U TLBINF32.OCX
 if errorlevel 1 pause
 
